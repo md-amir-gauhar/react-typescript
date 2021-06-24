3911f744b4d4aaf7c6188886cd2f6691d952b38d
md-amir-gauhar <gauharamir15@gmail.com>
2021-06-24 23:23:34 +0530

    types union in state

---
+++ src/state/UserSearch.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+const users = [
+  { name: 'Sarah', age: 20 },
+  { name: 'Alex', age: 18 },
+  { name: 'Micheal', age: 22 },
+];
+
+const UserSearch: React.FC = () => {
+  const [name, setName] = useState('');
+  const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name === name;
+    });
+    setUser(foundUser);
+  };
+  return (
+    <div>
+      <h3>User Search</h3>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={onClick}>Find User</button>
+      <div>
+        {user && user.name}
+        {user && user.age}
+      </div>
+    </div>
+  );
+};
+
+export default UserSearch;
